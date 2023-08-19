eeadd2c814296c8e8cf55954d355f845ca5686d3
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-19 19:12:50 +0530

    var let const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId =144553;
+let accountEmail = "account";
+var accountPassword = "123445";  
+accountCity="jaipur";
+let accountState;
+// accountId =2;
+
+accountEmail ="email";
+accountPassword="password";
+accountCity="city";
+// console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState]);
+
+// ┌─────────┬────────────┐
+// │ (index) │   Values   │
+// ├─────────┼────────────┤
+// │    0    │   144553   │
+// │    1    │  'email'   │
+// │    2    │ 'password' │
+// │    3    │   'city'   │
+// │    4    │ undefined  │
+// └─────────┴────────────┘
