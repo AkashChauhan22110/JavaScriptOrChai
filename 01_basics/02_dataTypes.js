75bcbd6568ddb059f6313f86cccdffd5ce5f051f
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-21 13:21:57 +0530

    done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -21,3 +21,21 @@ let isLoggedIn = false; //boolean
 
 
 console.log(typeof"hitesh") //string type
+
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
