090bde09da4ddf89b7938ea40e732cada9265b51
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-21 16:11:51 +0530

    done

--- 01_basics/03_conversionOpertion.js
+++ 01_basics/03_conversionOpertion.js
@@ -1,22 +1,29 @@
-let score = 33;
-console.log(typeof score); //number
-console.log(typeof (score)); //number
+// let score = 33;
+// console.log(typeof score); //number
+// console.log(typeof (score)); //number
 
-let score1 = "33";
-console.log(typeof  score1); //string
+// let score1 = "33";
+// console.log(typeof  score1); //string
 
-let score2 = "33";
-console.log(typeof + score2); //number
+// let score2 = "33";
+// console.log(typeof + score2); //number
 
-let score5 = "33adsfds";
-let valueInNumber = Number(score5);
-console.log(typeof valueInNumber); //number
-console.log(valueInNumber); //NaN
+// let score5 = "33adsfds";
+// let valueInNumber = Number(score5);
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn); //true
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); //true
 
-let isLoggedIn1 = 0;
-let booleanIsLoggedIn1=Boolean(isLoggedIn1);
-console.log(booleanIsLoggedIn1); //false
+// let isLoggedIn1 = 0;
+// let booleanIsLoggedIn1=Boolean(isLoggedIn1);
+// console.log(booleanIsLoggedIn1); //false
+
+
+// **********************Operations************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue); //-3
