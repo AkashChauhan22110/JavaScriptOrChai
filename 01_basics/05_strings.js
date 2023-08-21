e2eec24f8069243ac40dc6cd2c28750edacbdc65
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-21 23:02:30 +0530

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+// const name = "Akash";
+// const repoCount = 50;
+// console.log(name + " repo count: " + repoCount); //Akash repo count: 50
+ 
+//String interpolation: we can always use`
+// console.log(`Hello my name is ${name} and My repo count is ${repoCount}`);
+// Hello my name is Akash and My repo count is 50
+
+//String Methods:
+let name = new String ("   AkashChauhan  ");
+// console.log(name.length); //12
+// console.log(name.toUpperCase()); //AKASHCHAUHAN
+// console.log(name.charAt(3)); //s
+// console.log(name.indexOf('u'));//8
+// console.log(name.substring(0,4));//Akas
+// console.log(name.slice(1,2));//k 
+// console.log(name.trim());//AkashChauhan
+// console.log(name.replace("n" , "_"));//   AkashChauha_  
+// console.log(name.includes("han"));//true
+// console.log(name.split("a"));//[ '   Ak', 'shCh', 'uh', 'n  ' ]
