f5efe0377a03afbb3723d044b3c0a4ae501f41ac
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-21 18:08:06 +0530

    DataTypes summary

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -38,3 +38,19 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
