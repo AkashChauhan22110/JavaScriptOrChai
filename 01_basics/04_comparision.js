9a95378d9b661ac45d08440d513e47594ff42a55
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-21 16:34:59 +0530

    done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);//true
+// console.log(2>=1);//true
+// console.log(2<1);//false
+// console.log(2==1);//false
+// console.log(2!=1);//true
+
+// console.log("2" > 1);//true
+// console.log("02" > 1);//true
+
+// console.log(null>0);//false
+// console.log(null<0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //false
