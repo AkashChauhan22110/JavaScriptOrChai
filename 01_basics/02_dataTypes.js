ee639076e01b81b0024c4706f88a4eaf7c065aaa
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-21 13:19:52 +0530

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //Treat all JS code as newer version.
+// alert(3+3); //Showing Error bcoz, we are using node js , not browser.
+//Code Readability should be high.
+// use mdn is very useful for concept of JavaScript.  
+
+
+// DataTypes:
+let name ="Akash";  //string dataTypes
+let age = 23; //number string
+let isLoggedIn = false; //boolean
+
+
+//number => 2 to 53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+//objects
+
+
+console.log(typeof"hitesh") //string type
