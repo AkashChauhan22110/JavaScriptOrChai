1b02362328d43eb3d7c86435464a17f771cc4fa2
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-22 16:02:28 +0530

    done

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -1,4 +1,4 @@
-let myDate = new Date();
+// let myDate = new Date();
 // console.log(myDate);//2023-08-22T09:43:08.816Z
 // console.log(myDate.toString());//Tue Aug 22 2023 15:13:42 GMT+0530 (India Standard Time)
 // console.log(myDate.toISOString());//2023-08-22T09:46:02.154Z
@@ -11,5 +11,43 @@ let myDate = new Date();
 // let myCreatedDate = new Date(2023, 7, 22);
 // console.log(myCreatedDate.toDateString()); //Tue Aug 22 2023 month starts with 0 index
 
-let myTimeStamp = Date.now();
-console.log(myTimeStamp);//1692698642444
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);//1692698642444
+
+
+
+
+
+
+
+
+
+// Dates
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date();
+// console.log(newDate);//2023-08-22T10:28:22.258Z
+// console.log(newDate.getMonth() + 1);//8
+// console.log(newDate.getDay());//2
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",  
+// })
