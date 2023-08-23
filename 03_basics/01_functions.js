9d654651787a117aa6bc612b057fae18bd87bdef
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-23 20:22:56 +0530

    function and object

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,10 +1,10 @@
-function sayMyName() {
-  console.log("A");
-  console.log("K");
-  console.log("A");
-  console.log("S");
-  console.log("H"); 
-}
+// function sayMyName() {
+//   console.log("A");
+//   console.log("K");
+//   console.log("A");
+//   console.log("S");
+//   console.log("H");
+// }
 // sayMyName();
 // A
 // K
@@ -32,4 +32,35 @@ function sayMyName() {
 // console.log(loginUserMessage("hitesh"))//hitesh just logged in
 // console.log(loginUserMessage(""))//PLease enter a username undefined
 
+// function calculateCartNumbers(...num1) {
+//   return num1;
+// }
+// console.log(calculateCartNumbers(2));//2
+// console.log(calculateCartNumbers(200,300,400));//[ 200, 300, 400 ]
 
+// const user = {
+//   userName: "user",
+//   price: 199,
+// };
+
+// function handleObject(anyObject) {
+//   console.log(
+//     `username is ${anyObject.userName} and price is ${anyObject.price}`
+//   );
+// }
+// handleObject(user); // username is user and price is 199
+
+//handleObject(user)
+// handleObject(user);
+// handleObject({
+//   userName: "Akash",
+//   price: 200,
+// }); //username is user and price is 199
+
+// const myNewArray = [200, 300,400];
+
+// function returnSecondValue(getArray) {
+//   return getArray[1]
+// }
+// // console.log(returnSecondValue(myNewArray));//300
+// console.log(returnSecondValue([500,600,700])); //600
