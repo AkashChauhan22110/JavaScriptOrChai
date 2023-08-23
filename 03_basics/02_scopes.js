2834aea0a40579289c00e65d6395098469077883
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-23 23:47:58 +0530

    scope level and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -17,3 +17,41 @@
 // console.log(c); //30 they showing result because of global scope
  
 // console.log(a);//200 //global scope
+
+
+//Scope level and hoisting in javascript.
+// function one() {
+//   const username = "Akash";
+
+//   function two() {
+//     const website = "youtube";
+//     console.log(username);
+//   }
+//   // console.log(website);
+//   two();//Akash
+// }
+// one();
+
+// if (true) {
+//   const username = "Akash";
+//   if (username === "Akash") {
+//     const website ='youtube';
+//     console.log(username);//Akash
+//   }
+//  // console.log(website);
+// }
+//  // console.log(username);
+
+// ======================Interesting======================
+function addOne(num1) {
+  return num1 + 1;
+}
+console.log(addOne(5));//6
+
+
+//hoisting if you use function hold in a variable you can display before the function
+// console.log(addTwo(10));//11
+const addTwo = function(num){ //expression: hold function in variable
+  return num + 1;
+}
+console.log(addTwo(10));//11
