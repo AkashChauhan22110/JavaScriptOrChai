7d004cff69ddc357aaf4e961a4da8b5da2882063
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-23 23:20:55 +0530

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+// let a =10;
+// const b =20;
+// var c =30;
+
+// console.log(a);//10
+// console.log(b);//20
+// console.log(c);//30
+
+// let a =200;
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   console.log(a);//10 //block scope
+// }
+// console.log(a); //10 they showing error because of block scope
+// console.log(b); //20 they showing error because of block scope
+// console.log(c); //30 they showing result because of global scope
+ 
+// console.log(a);//200 //global scope
