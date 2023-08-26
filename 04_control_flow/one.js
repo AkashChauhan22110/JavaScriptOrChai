05b9f911d36c813aba7e46e299e9294f84ab5434
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-26 17:58:02 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,64 @@
+//Control Flow / logic Flow
+
+//if statement
+// if (2 == "2") { //check only values
+//   console.log("Its executed ");//Its executed
+// }
+// else {
+//   console.log("It's not executed ");
+// }
+
+// if (2 === "2") { //check values and datatypes so condition id false so this belongs to else
+//   console.log("Its executed ");
+// }
+// else {
+//   console.log("It's not executed ");//It's not executed
+// }
+
+// const temperature = 43;
+// if (temperature <= 40) {
+//   console.log("Temperature is less than 40");
+// } else {
+//   console.log("Temperature is greater than 40");
+// }
+// //Temperature is greater than 40
+
+// const score =200;
+// if (score > 100) {
+//   const power = 'fly';
+//   console.log(`Ready to ${power}`);//Ready to fly
+// }
+// // console.log(`Ready to ${power}`);//power is not defined block scoping error
+
+// // shorthand notation
+// const balance = 1000;
+// // if (balance > 500) console.log("test1"),console.log("test2") //test1 test2
+
+// if (balance < 500) {
+//   console.log("Less than 500");
+// } else if (balance < 750) {
+//   console.log("Less than 750");
+// } else if (balance < 900) {
+//   console.log("Less than 900");
+// } else {
+//   console.log("Less than 1200"); //Less than 1200
+// }
+
+const userLoggedIn =true;
+const debitCard = false;
+
+// if (userLoggedIn && debitCard) {
+// console.log("Allow to user");    //And statement check both conditions
+// }
+// else{
+//   console.log("Make your Login access or debit card");
+// }
+// Make your Login access or debit card
+
+// if (userLoggedIn || debitCard) {
+//   console.log("Allow to user");    //Or statement check one condition and pass away.
+// }
+// else{
+//   console.log("Make your Login access or debit card");
+// }
+// //Allow to user
