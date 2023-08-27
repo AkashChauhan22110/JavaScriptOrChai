0d1d76189af638c583639105aefbabd2e84958dd
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-27 21:04:15 +0530

    while do while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,35 @@
+// while loop
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`value of index is ${index}`);
+//   index = index + 2;
+// }
+// // value of index is 0
+// // value of index is 2
+// // value of index is 4
+// // value of index is 6
+// // value of index is 8
+// // value of index is 10
+
+
+// let myArray = ['flash', "batman", "superman"]
+// let arr =0;
+// while (arr < myArray.length) {
+//      console.log(`Value is ${myArray[arr]}`);
+//      arr = arr + 1
+// }
+// // Value is flash
+// // Value is batman
+// // Value is superman
+
+// let val =5;
+// do {
+//      console.log(`index value is ${val}`);
+//      val++;
+// } while (val <= 10);
+// // index value is 5
+// // index value is 6
+// // index value is 7
+// // index value is 8
+// // index value is 9
+// // index value is 10
