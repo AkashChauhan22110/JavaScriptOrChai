8188acdb176e0ef877a0af3427e8ca7c8d727c03
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-28 18:37:45 +0530

    reduce

---
+++ 05_iterations/08_eight.js
@@ -0,0 +1,27 @@
+//Reduce
+
+// const myNums =[1,2,3,4,5];
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+// console.log(myTotal);
+// // acc: 0 and currval: 1
+// // acc: 1 and currval: 2
+// // acc: 3 and currval: 3
+// // acc: 6 and currval: 4
+// // acc: 10 and currval: 5
+// // 15
+
+// const sum = myNums.reduce((acc, currval) =>{
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+// return acc + currval;
+// },0);
+// console.log(sum);
+// // acc: 0 and currval: 1
+// // acc: 1 and currval: 2
+// // acc: 3 and currval: 3
+// // acc: 6 and currval: 4
+// // acc: 10 and currval: 5
+// // 15
+
