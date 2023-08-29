12da457b907c354f0d5edf07e8c275501f2f7112
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-29 14:39:14 +0530

    dom

---
+++ 06_dom/01_one.js
@@ -0,0 +1,8 @@
+/* Window Objects >> Document >> Html >> Head >> 1.Meta/2.Title
+Body >> 1.Div */
+
+
+//Code for a sum of two numbers in js
+// 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
+
+//What is redux
