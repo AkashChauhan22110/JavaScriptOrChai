22d501e32775093b5abaebb397c060e8cc960d1c
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-30 17:27:08 +0530

    DOM Manipulation

--- 06_dom/01_one.js
+++ 06_dom/01_one.js
@@ -1,8 +1,2 @@
 /* Window Objects >> Document >> Html >> Head >> 1.Meta/2.Title
 Body >> 1.Div */
-
-
-//Code for a sum of two numbers in js
-// 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
-
-//What is redux
